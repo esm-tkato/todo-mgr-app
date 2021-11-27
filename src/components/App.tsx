ec88ef8e13528aa5040ee47139eab6749fd3cb7e
加藤友彦 <tomamitu@katoutomohikonoMacBook-Pro.local>
2021-11-27 11:00:53 +0900

    setup react context provider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,17 +3,19 @@ import { Container } from 'react-bootstrap'
 
 import TaskForm from './TaskForm'
 import Tasks from './Tasks'
-
+import AppContext from '../contexts/AppContext'
 import reducer from '../reducers'
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
 
   return (
-    <Container fluid>
-      <TaskForm state={state} dispatch={dispatch} />
-      <Tasks state={state} dispatch={dispatch} />
-    </Container>
+    <AppContext.Provider value={'aaa'}>
+      <Container fluid>
+        <TaskForm state={state} dispatch={dispatch} />
+        <Tasks state={state} dispatch={dispatch} />
+      </Container>
+    </AppContext.Provider>
   )
 }
 
