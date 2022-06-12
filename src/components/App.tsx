b0bc6917eaf9ac7c9d826b86b72992aeb647e90b
加藤友彦 <tomamitu@katoutomohikonoMacBook-Pro.local>
2022-06-12 21:08:45 +0900

    initialStateに設定してみる

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,9 +7,37 @@ import Tasks from './Tasks'
 import { StateContext, DispatchContext } from '../contexts/AppContext'
 import reducer from '../reducers'
 // import { INIT_TASK } from '../actions'
+import { task } from '../reducers'
+import axios from 'axios'
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, [])
+  let todos: task[] = []
+  function get_data() {
+    return new Promise((resolve, reject) => {
+        axios.get('http://localhost:3001/todos')
+            .then(response => {
+              let result: task[] = response.data;
+              console.log('result):', result)
+              resolve(result);
+            })
+            .catch(error => {
+                reject(error);
+            })
+            .then(() => {
+                // 特になし
+            })
+    })
+  }
+  async function get_tasks() {
+    todos = await get_data() as task[];    
+  }
+
+  get_tasks();
+
+  // const initialState = {
+  //   state: todos
+  // }
+  const [state, dispatch] = useReducer(reducer, todos);
 
   // useEffect(() => {
   //   dispatch({ type: INIT_TASK})
